881c7d5131dfdec2d8ff75cefc217cb24fa05d4a
truposchet <art.shaper1@gmail.com>
2018-05-16 02:40:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tom/ecgexample/MainActivity.java
@@ -0,0 +1,332 @@
+/*
+ * Copyright 2016 AndroidPlot.com
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.example.tom.ecgexample;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.androidplot.Plot;
+import com.androidplot.util.Redrawer;
+import com.androidplot.xy.*;
+import com.example.tom.ecgexample.R;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ref.*;
+import java.lang.reflect.Method;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+/**
+ * An example of a real-time plot displaying an asynchronously updated model of ECG data.  There are three
+ * key items to pay attention to here:
+ * 1 - The model data is updated independently of all other data via a background thread.  This is typical
+ * of most signal inputs.
+ *
+ * 2 - The main render loop is controlled by a separate thread governed by an instance of {@link Redrawer}.
+ * The alternative is to try synchronously invoking {@link Plot#redraw()} within whatever system is updating
+ * the model, which would severely degrade performance.
+ *
+ * 3 - The plot is set to render using a background thread via config attr in  R.layout.ecg_example.xml.
+ * This ensures that the rest of the app will remain responsive during rendering.
+ */
+public class MainActivity extends Activity {
+    private XYPlot plot;
+
+    /**
+     * Uses a separate thread to modulate redraw frequency.
+     */
+    private Redrawer redrawer;
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private static String address = "20:16:03:08:53:65";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();       // get Bluetooth adapter
+        checkBTState();
+        // Set up a pointer to the remote node using it's address.
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+        // Two things are needed to make a connection:
+        //   A MAC address, which we got above.
+        //   A Service ID or UUID.  In this case we are using the
+        //     UUID for SPP.
+
+        try {
+            btSocket = createBluetoothSocket(device);
+        } catch (IOException e) {
+            errorExit("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
+        }
+
+        // Discovery is resource intensive.  Make sure it isn't going on
+        // when you attempt to connect and pass your message.
+        btAdapter.cancelDiscovery();
+
+        // Establish the connection.  This will block until it connects.
+        try {
+            btSocket.connect();
+        } catch (IOException e) {
+            try {
+                btSocket.close();
+            } catch (IOException e2) {
+                errorExit("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
+            }
+        }
+
+
+
+        setContentView(R.layout.ecg_example);
+
+        // initialize our XYPlot reference:
+        plot = (XYPlot) findViewById(R.id.plot);
+
+        ECGModel ecgSeries = new ECGModel(2000, 200, btSocket );
+
+        // add a new series' to the xyplot:
+        MyFadeFormatter formatter =new MyFadeFormatter(2000);
+        formatter.setLegendIconEnabled(false);
+        plot.addSeries(ecgSeries, formatter);
+        plot.setRangeBoundaries(0, 10, BoundaryMode.FIXED);
+        plot.setDomainBoundaries(0, 2000, BoundaryMode.FIXED);
+
+        // reduce the number of range labels
+        plot.setLinesPerRangeLabel(3);
+
+        // start generating ecg data in the background:
+        ecgSeries.start(new WeakReference<>(plot.getRenderer(AdvancedLineAndPointRenderer.class)));
+
+        // set a redraw rate of 30hz and start immediately:
+        redrawer = new Redrawer(plot, 30, true);
+    }
+
+    private void checkBTState() {
+        // Check for Bluetooth support and then check to make sure it is turned on
+        // Emulator doesn't support Bluetooth and will return null
+        if(btAdapter==null) {
+            errorExit("Fatal Error", "Bluetooth not support");
+        } else {
+            if (btAdapter.isEnabled()) {
+
+                Toast.makeText(this, "BtOn", Toast.LENGTH_SHORT);
+            } else {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
+        }
+    }
+
+    private void errorExit(String title, String message){
+        Toast.makeText(getBaseContext(), title + " - " + message, Toast.LENGTH_LONG).show();
+        finish();
+    }
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        try {
+            final Method m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", new Class[] { UUID.class });
+            return (BluetoothSocket) m.invoke(device, MY_UUID);
+        } catch (Exception e) {
+
+        }
+
+        return  device.createRfcommSocketToServiceRecord(MY_UUID);
+    }
+
+    /**
+     * Special {@link AdvancedLineAndPointRenderer.Formatter} that draws a line
+     * that fades over time.  Designed to be used in conjunction with a circular buffer model.
+     */
+    public static class MyFadeFormatter extends AdvancedLineAndPointRenderer.Formatter {
+
+        private int trailSize;
+
+        public MyFadeFormatter(int trailSize) {
+            this.trailSize = trailSize;
+        }
+
+        @Override
+        public Paint getLinePaint(int thisIndex, int latestIndex, int seriesSize) {
+            // offset from the latest index:
+            int offset;
+            if(thisIndex > latestIndex) {
+                offset = latestIndex + (seriesSize - thisIndex);
+            } else {
+                offset =  latestIndex - thisIndex;
+            }
+
+            float scale = 255f / trailSize;
+            int alpha = (int) (255 - (offset * scale));
+            getLinePaint().setAlpha(alpha > 0 ? alpha : 0);
+            return getLinePaint();
+        }
+    }
+
+    /**
+     * Primitive simulation of some kind of signal.  For this example,
+     * we'll pretend its an ecg.  This class represents the data as a circular buffer;
+     * data is added sequentially from left to right.  When the end of the buffer is reached,
+     * i is reset back to 0 and simulated sampling continues.
+     */
+    public static class ECGModel implements XYSeries {
+
+        private final Number[] data;
+        private final long delayMs;
+        private final int blipInteral;
+        private final Thread thread;
+        private boolean keepRunning;
+        private int latestIndex;
+        private final InputStream mmInStream;
+        private float inByte = 0;
+        private byte[] buffer = new byte[10];  // buffer store for the stream
+
+        private WeakReference<AdvancedLineAndPointRenderer> rendererRef;
+
+        /**
+         *
+         * @param size Sample size contained within this model
+         * @param updateFreqHz Frequency at which new samples are added to the model
+         */
+        public ECGModel(int size, int updateFreqHz, BluetoothSocket socket) {
+
+            InputStream tmpIn = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+            } catch (IOException e) {}
+
+            mmInStream = tmpIn;
+
+            data = new Number[size];
+
+
+            for(int i = 0; i < data.length; i++) {
+                data[i] = 0;
+            }
+
+            // translate hz into delay (ms):
+            delayMs = 1000 / updateFreqHz;
+
+            // add 7 "blips" into the signal:
+            blipInteral = size / 7;
+
+
+            thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        int bytes;
+                        while (keepRunning) {
+                            try {
+                                if (latestIndex >= data.length) {
+                                    latestIndex = 0;
+                                }
+
+                                // generate some random data:
+                                ////////////////////////////////////
+                                bytes = mmInStream.read(buffer);        // Get number of bytes and message in "buffer"
+                                String str = new String(buffer, StandardCharsets.UTF_8);
+                                //System.out.println(str);
+                                str = str.replace(System.getProperty("line.separator"), " ");
+
+                                String inString = str.trim();
+                                String[] array = inString.split(" ");
+                                //Toast.makeText(SimpleXYPlotActivity.this, array[0], Toast.LENGTH_SHORT).show();
+                                System.out.println(array[0]);
+                                inByte = Float.parseFloat(array[0]);
+                                data[latestIndex] = inByte;
+                                ///////////////////////////////////
+
+                                if (latestIndex < data.length - 1) {
+                                    // null out the point immediately following i, to disable
+                                    // connecting i and i+1 with a line:
+                                    data[latestIndex + 1] = null;
+                                }
+
+                                if (rendererRef.get() != null) {
+                                    rendererRef.get().setLatestIndex(latestIndex);
+                                    Thread.sleep(delayMs);
+                                } else {
+                                    keepRunning = false;
+                                }
+                                latestIndex++;
+                            }catch (IOException e) {
+                                break;
+                            }
+                        }
+                    } catch (InterruptedException e) {
+                        keepRunning = false;
+                    }
+                }
+            });
+        }
+
+        public void start(final WeakReference<AdvancedLineAndPointRenderer> rendererRef) {
+            this.rendererRef = rendererRef;
+            keepRunning = true;
+            thread.start();
+        }
+
+        @Override
+        public int size() {
+            return data.length;
+        }
+
+        @Override
+        public Number getX(int index) {
+            return index;
+        }
+
+        @Override
+        public Number getY(int index) {
+            return data[index];
+        }
+
+        @Override
+        public String getTitle() {
+            return "Signal";
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        redrawer.finish();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        try     {
+            btSocket.close();
+        } catch (IOException e2) {
+            errorExit("Fatal Error", "In onPause() and failed to close socket." + e2.getMessage() + ".");
+        }
+    }
+}
